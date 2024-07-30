c44616ee41d08dff831667bb7c90a1750eb183a3
eZWALT <waltertheshadow333@gmail.com>
2024-07-30 12:42:33 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::net::TcpListener; 
+use std::io::Write;
 
 fn main() {
     println!("Logs from your program will appear here!");
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
+                stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
                 println!("accepted new connection");
             }
             Err(e) => {
