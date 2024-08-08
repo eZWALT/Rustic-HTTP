af9182de40bf618c81a4335a15548226347a95e4
Walter Jose Troiani Vargas <waltertheshadow333@gmail.com>
2024-08-08 22:49:29 +0200

    read headers and user-agent endpoint implemented

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,29 @@ use std::net::{TcpListener, TcpStream};
 use std::io::{Write, BufRead, BufReader, Read};
 use std::collections::HashMap;
 
+/*
+enum HTTPMethod {
+    GET, 
+    POST, 
+    PUT, 
+    DELETE
+}
+
+enum ContentType {
+    PLAIN,
+    JSON,
+}
+
+struct HTTPResponse {
+    version: String,
+    status_code: u16,
+    status_msg: String, 
+    headers: HashMap<String, String>,
+    body: String,
+    content_ty: ContentType
+}
+*/
+
 struct HTTPRequest {
     method: String,
     path: String,
@@ -36,7 +59,6 @@ impl HTTPRequest {
         }
         self.method = parts[0].to_string();
         self.path = parts[1].to_string();
-        //println!("The path of this request is: {}", self.path);
         self.version = parts[2].to_string();
 
         // Read the headers into a vector
@@ -62,7 +84,7 @@ impl HTTPRequest {
         }
 
         // Read the optional body
-        if let Some(length_str) = self.headers.get("Content-Length") {
+        if let Some(_length_str) = self.headers.get("Content-Length") {
             /*
             let length: usize = match length_str.parse() {
                 Ok(len) => len,
@@ -88,13 +110,17 @@ impl HTTPRequest {
     }
 }
 
+//This function encapsulates the behaviour of the HTTP server
 fn write_response(request: &HTTPRequest, stream: &mut TcpStream) -> Result<(), std::io::Error> {
     //By default this will be the default response
-    let is_echo_req: bool = request.path.as_str().starts_with("/echo");
+    let path = request.path.as_str();
+    let is_echo_endpoint: bool = path.starts_with("/echo");
+    let is_agent_endpoint: bool = path.starts_with("/user-agent");
 
-    let (status_code, status_msg) = match request.path.as_str() { 
+    let (status_code, status_msg) = match path { 
         "/" => (200, "OK"),
-        s if s.starts_with("/echo") => (200, "OK"),
+        _ if is_echo_endpoint => (200, "OK"),
+        _ if is_agent_endpoint => (200, "OK"),
         _ => (404, "Not Found"),
     };
 
@@ -106,22 +132,44 @@ fn write_response(request: &HTTPRequest, stream: &mut TcpStream) -> Result<(), s
     );
 
     let mut response = status.to_string();
-    let body = request.path.as_str().trim_start_matches("/echo/");
+    let mut body = String::new();
 
-    if is_echo_req {
+    if is_echo_endpoint {
         let mut headers = HashMap::new();
         headers.insert("Content-Type", "text/plain".to_string());
+        body.push_str(path.trim_start_matches("/echo/"));
         headers.insert("Content-Length", body.len().to_string());
     
-    
-        for (key, value) in headers {
+        for (key, value) in &headers {
             response.push_str(&format!("{}: {}\r\n" ,key, value));
         }
-        //println!("{}", response);
+
+
+    }
+    else if is_agent_endpoint {
+        let mut headers = HashMap::new();
+
+        headers.insert("Content-Type", "text/plain".to_string());
+
+        if let Some(user_agent) = request.headers.get("User-Agent") {
+            headers.insert("User-Agent", user_agent.to_string());
+            let user_str: &str = user_agent.as_str();
+            body.push_str(user_str);
+            headers.insert("Content-Length", body.len().to_string());
+            
+            for (key, value) in &headers {
+                response.push_str(&format!("{}: {}\r\n" ,key, value));
+            }
+
+        }
+        else {
+            return Err(std::io::Error::new(std::io::ErrorKind::NotFound, "User-Agent header not found"));
+        }
+
     }
 
     response.push_str("\r\n");
-    response.push_str(body);
+    response.push_str(body.as_str());
 
     stream.write_all(response.as_bytes())?;
     Ok(())
